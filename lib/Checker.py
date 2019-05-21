17c2004a6b6fc24ced59be805b689791ad0eecfb
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2019-05-21 10:11:21 +0200

    Change Solution init

--- lib/Checker.py
+++ lib/Checker.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, copy
 from lib.Probleme import *
 from lib.Utils import *
 
@@ -35,8 +35,39 @@ class Ressource(object):
   def __repr__(self):
     return str(self.origin)+" -> "+str(self.destination)+" { dueDate: "+str(self.dueDate)+", length : "+str(self.length)+", capacity : "+str(self.capacity)+",listBlock : "+str(self.listBlock)+"}"
 
+
 class Solution(object):
-  def __init__(self,source_file):
+  def __init__(self,fichierProb="",listNoeud=[],tauxNoeud=[]):
+    self.file=fichierProb
+    if self.file != "":
+      self.probleme = Probleme(self.file)
+      self.safeNode = self.probleme.idSafeNode
+      self.nodeIni = []
+      for i in range(len(listNoeud)):
+        self.nodeIni.append({"id": listNoeud[i], "rate": int(tauxNoeud[i]), "tDebut": 0})
+      self.ressources = {}
+      for arc in self.probleme.edges.items():
+        # Creating id on dic
+        self.ressources[arc[0]] = (Ressource(arc[1]))
+      tmp=[]
+      for c in self.nodeIni:
+        for node in self.probleme.evacuationPath:
+          if node.idNode == c["id"]:
+            c["population"]=node.pop
+            c["maxRate"]=node.maxRate
+            c["path"]=node.path
+            tmp.append(CompleteNode(c))
+      self.nodeIni = tmp
+    else:
+      self.probleme = None
+      self.safeNode = None
+      self.nodeIni = []
+      self.ressources = {}
+    self.tObjectif = math.inf
+    self.metadata = []
+    self.timeCalc = 0
+
+  def load(self,source_file):
     header = True
     headerSize = 0
     with open(source_file) as f:
@@ -59,21 +90,29 @@ class Solution(object):
         self.ressources[arc[0]] = (Ressource(arc[1]))
       self.isValid = True if f.readline().strip().lower() == "valid" else False
       tmp=[]
-      for node in self.probleme.evacuationPath:
-        for c in self.nodeIni:
+      for c in self.nodeIni:
+        for node in self.probleme.evacuationPath:
           if node.idNode == c["id"]:
-            c["population"]=node.pop
-            c["maxRate"]=node.maxRate
-            c["path"]=node.path
+            c["population"] = node.pop
+            c["maxRate"] = node.maxRate
+            c["path"] = node.path
             tmp.append(CompleteNode(c))
       self.nodeIni = tmp
       self.tObjectif = f.readline()
       self.timeCalc = int(f.readline())
       self.metadata = f.readlines()
       self.metadata = [ x.strip() for x in self.metadata ]
-
+      return self
+    
   def __repr__(self):
     return "File : " + str(self.file) + "\nnodeIni: " + str(self.nodeIni) + "\nressources: "+str(self.ressources).replace("]},","]},\n")+", tObjectif : "+str(self.tObjectif)+", timeCalc : "+str(self.timeCalc)+", metadata : "+str(self.metadata)+"\n"
+
+  def computeStartTime(self):
+    ressourceTmp = copy.deepcopy(self.ressources)
+    for node in self.nodeIni:
+      print(node)
+      for arc in node.path:
+        print(arc)
 
   def verify(self):
     # Creating Blocks
@@ -104,3 +143,4 @@ class Solution(object):
 
       # Vérifier que tmax respecté dans la solution
     return tMax != self.tObjectif
+
