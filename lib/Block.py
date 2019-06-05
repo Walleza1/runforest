81ddfe2f406b2826ba3a7075e8bfe41950ccac23
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2019-06-05 19:27:25 +0200

    CompleteNode calcul rate by min

--- lib/Block.py
+++ lib/Block.py
@@ -1,9 +1,10 @@
 class Block(object):
-  def __init__(self,tBegin,tEnd,flow,temp=False):
+  def __init__(self,tBegin,tEnd,flow,NodeOriginId,temp=False):
     self.tBegin=tBegin
     self.tEnd=tEnd
     self.flow=flow
     self.temp=temp
+    self.nodeOrigin=NodeOriginId
 
   def __repr__(self):
-    return "Block : {tBegin :"+str(self.tBegin)+",tEnd :"+str(self.tEnd)+", flow "+str(self.flow)+"}"
+    return "Block : [ tBegin :{}, tEnd :{}, flow :{}, origin:{} ]".format(self.tBegin,self.tEnd,self.flow,self.nodeOrigin)
--- lib/CompleteNode.py
+++ lib/CompleteNode.py
@@ -1,3 +1,5 @@
+from lib.Utils import *
+
 class CompleteNode(object):
   def __init__(self,dic={}):
     if any(dic):
@@ -8,14 +10,22 @@ class CompleteNode(object):
       self.path = dic["path"]
       self.tDebut = dic["tDebut"]
 
-  def load(self, node):
+  def load(self, node, edges):
     self.id = node.idNode
-    self.rate = node.maxRate
     self.population = node.pop
     self.maxRate = node.maxRate
     self.path = node.path
     self.tDebut = 0
+    
+    # Determiner le rate.
+    # Le max rate peut faire exploser un arc. 
+    # Faut choisir le min entre le min sur le chemin et le maxRate
+    tmp = []
+    for arc in self.path:
+      tmp.append(edges[arc].capacity)
+    minChemin=min(tmp)
+    self.rate = min(minChemin,node.maxRate)
     return self
 
   def __repr__(self):
-    return "{ id : "+str(self.id)+", rate : "+str(self.rate)+", maxRate : "+str(self.maxRate)+", path : "+str(self.path)+", tDebut (d'évac) : "+str(self.tDebut)+"\n"
+    return "{ id : "+str(self.id)+", rate : "+str(self.rate)+", maxRate : "+str(self.maxRate)+", path : "+str(self.path)+", tDebut (d'évac) : "+str(self.tDebut)+"}"
